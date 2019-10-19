f9c0f1c14b791ac5d2b98c54aac4bfae83d39582
Audeck <55060124+Audeck@users.noreply.github.com>
2019-10-19 15:47:46 +0200

    Restructured into a function, cosmetic changes

--- FykosBlackPercentage.py
+++ FykosBlackPercentage.py
@@ -1,13 +1,16 @@
-from PIL import Image                       #Thanks, pillow!
+from PIL import Image                       # Thanks, pillow!
 
-im = Image.open("FYKOSpdf1200.png")         #Opening the png file (don't have to save afterwards as I'm not editing it in any way)
-(width, height) = im.size                   #Setting width and height as variables for comfort (completely arbitrary; though might be worth not calling the library's function over and over - would have to measure the timing (at some point new local variables might be worth setting))
-pixArray = im.load()                        #Loading the pixel values into what I believe is a 2D array (or something related to it)
-blackPixCount = 0                           #Black pixel tally
+def calculateColor(image, color):
+    im = Image.open(image)                  # Opening the png file (don't have to save afterwards as I'm not editing it in any way)
+    (width, height) = im.size               # Setting width and height as variables for comfort
+    pixArray = im.load()                    # Loading the pixel values into what I believe is a 2D array (or something related to it)
+    blackPixCount = 0                       # Colored pixel tally
 
-for y in range(0,height):                   #Iterate over every Y coordinate
-    for x in range(0,width):                #Iterate over every X coordinate on a given Y coordinate
-        if pixArray[x,y] == (0,0,0):        #Checking if pixel value == black
-            blackPixCount += 1              #If so, the adding one to the black pixel tally
+    for y in range(0,height):               # Iterate over every Y coordinate
+        for x in range(0,width):            # Iterate over every X coordinate on a given Y coordinate
+            if pixArray[x,y] == color:      # Checking if pixel value == color
+                blackPixCount += 1          # If so, the adding one to the colored pixel tally
 
-print(blackPixCount/(width*height))         #Print out the final percentage of black pixels/all pixels
+    return blackPixCount/(width*height)
+
+print(str(calculateColor("FYKOSpdf1200.png", (0,0,0)) * 100) + "%") # Print out the final percentage
