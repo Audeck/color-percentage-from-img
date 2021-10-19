bc8aa4f2fbb7beca6ae089024882fec2a582b456
audeck <55060124+audeck@users.noreply.github.com>
2021-10-19 03:01:29 +0200

    Update ColorPercentageCalculator.py

--- ColorPercentageCalculator.py
+++ ColorPercentageCalculator.py
@@ -4,14 +4,14 @@ def calculateColor(image, color):
     im = Image.open(image)
     (width, height) = im.size
     pixArray = im.load()
-    blackPixCount = 0
+    colorPixCount = 0
 
     for y in range(0, height):  # Iterate over every Y coordinate
         for x in range(0, width):  # Iterate over every X coordinate on a given Y coordinate
             if pixArray[x, y] == color:
-                blackPixCount += 1
+                colorPixCount += 1
 
-    return blackPixCount/(width*height)
+    return colorPixCount/(width*height)  # Return certain-color pixel ratio
 
 if __name__ == "__main__":
-    print(str(calculateColor("FYKOSpdf1200.png", (0, 0, 0)) * 100) + "%")  # Print out the final percentage
+    print(str(calculateColor("FYKOSpdf1200.png", (0, 0, 0)) * 100) + "%")  # Example
